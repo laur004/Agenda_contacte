b80a0ad30d7d0ebe214fc52264dea8274224fb30
DESKTOP-RR7SAFR\Xtreme Pc <sarighioleanulaurentiu@gmail.com>
2024-03-23 23:14:09 +0200

    tema 1

--- main.cpp
+++ main.cpp
@@ -13,6 +13,7 @@ public:
     ~Contact();
     Contact();
     Contact(std::string nume, std::string prenume, std::string numar_telefon);
+    Contact(const Contact& other);
     Contact& operator=(const Contact& other);
 
     void afisare_contact() const;
@@ -32,10 +33,18 @@ public:
     bool operator>(const Contact& other) const;
 };
 
-Contact::~Contact()=default;
+Contact::~Contact(){
+    std::cout << "Contactul a fost sters cu succes!\n";
+}
 
 Contact::Contact() : nume("contact"), prenume("anonim"), numar_telefon("fara numar") {}
 Contact::Contact(std::string nume, std::string prenume, std::string numar_telefon) : nume(std::move(nume)), prenume(std::move(prenume)), numar_telefon(std::move(numar_telefon)) {}
+Contact::Contact(const Contact& other) {
+    nume = other.nume;
+    prenume = other.prenume;
+    numar_telefon = other.numar_telefon;
+}
+
 
 Contact& Contact::operator=(const Contact& other) {
     if (this != &other) {
@@ -182,7 +191,6 @@ void Lista_Contacte::eliminare_contact() {
     }
 
     lista_contacte.erase(lista_contacte.begin() + indice - 1);
-    std::cout << "Contactul a fost sters cu succes!\n";
 }
 
 
